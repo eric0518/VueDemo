cebe7e661a67f93ebac2f2b7bbd8b39c66ac428d
xiaogangwang <xiaogangwang@yeah.net>
2019-06-21 18:47:44 +0800

    Updated struct.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/vueForm", function(req, res, next){
+  console.log("msg = " + req.query.msg);
+  console.log("multiMsg = " + req.query.multiMsg);
+  console.log("c1 = " + req.query.c1);
+  console.log("radio1 = " + req.query.radio1)
+  console.log("radio2 = " + req.query.radio2)
+  console.log("radio2 = " + req.query.radio3)
+  console.log("selected = " + req.query.select)
+  console.log("multiSelected = " + req.query.multiSelect)
+  res.render("index", {title:"submit succeed."})
+})
+
 module.exports = router;
